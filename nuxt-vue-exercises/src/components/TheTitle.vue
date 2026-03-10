b06a3cde87eaf472f452651a6f36efe15c364c7c
Juan Atahona <juanatahonadev@gmail.com>
2026-03-10 16:48:58 +0100

    feat(src/App.vue, components/TheTitle.vue): cambiar el metodo de recibiar los datos del padre, de props al slot, dejando cabida a un dato por defecto

--- nuxt-vue-exercises/src/components/TheTitle.vue
+++ nuxt-vue-exercises/src/components/TheTitle.vue
@@ -1,14 +1,5 @@
-<script setup lang="ts">
-withDefaults(
-  defineProps<{
-    propTitle?: string;
-  }>(),
-  {
-    propTitle: "A normal title",
-  },
-);
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <h1>{{ propTitle }}</h1>
+  <slot>A normal title</slot>
 </template>
