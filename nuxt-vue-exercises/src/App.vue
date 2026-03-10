4453d67819730f1054f296fd477498917edc5aed
Juan Atahona <juanatahonadev@gmail.com>
2026-03-10 16:43:47 +0100

    feat(src/App.vue,components/TheTitle.vue): crear un componente hijo que recibira los props del padre y los mostrara en caso de no llegar datos se pondra un valor por defecto

--- nuxt-vue-exercises/src/App.vue
+++ nuxt-vue-exercises/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-    
+import TheTitleVue from "./components/TheTitle.vue";
 </script>
 
 <template>
-    
+  <TheTitleVue prop-title="A good title"></TheTitleVue>
 </template>
