41f0616147ae654b0842d6808e040b6b2854a1de
Juan Atahona <juanatahonadev@gmail.com>
2026-03-11 09:56:46 +0100

    feat(app/components/TheCounter): dar limitaciones al contador para que solo se permita en un rango 0 > x <= 10

--- nuxt-vue-exercises/app/components/TheCounter.vue
+++ nuxt-vue-exercises/app/components/TheCounter.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
 const count = ref(0);
+
+const plusOneCounter = () => {
+  if (count.value >= 10 ) return;
+  count.value++;
+}
+
+const minusOneCounter = () => {
+  if (count.value <= 0 ) return;
+  count.value--;
+}
+
 </script>
 
 <template>
@@ -10,10 +21,14 @@ const count = ref(0);
       <TheTitle>El contador dinámico</TheTitle>
     </div>
     <div class="flex justify-center gap-3 pt-3">
-      <button class="bg-red-300 px-3 py-1 rounded hover:bg-red-950 hover:text-red-200" @click="count--">
+      <button class="bg-red-300 px-3 py-1 rounded hover:bg-red-950 hover:text-red-200 disabled:bg-gray-800 disabled:text-white disabled:cursor-not-allowed" @click="minusOneCounter"
+      :disabled="count <= 0"
+      >
         Decrement
       </button>
-      <button class="bg-blue-300 px-3 py-1 rounded hover:bg-blue-950 hover:text-blue-200" @click="count++">
+      <button class="bg-blue-300 px-3 py-1 rounded hover:bg-blue-950 hover:text-blue-200 disabled:bg-gray-800 disabled:text-white disabled:cursor-not-allowed" @click="plusOneCounter"
+      :disabled="count >= 10"
+      >
         Increment
       </button>
     </div>
